51c26c23acd44d6cd3ae7ccf523b566fe2e1fd62
SergeyBordulev <bordulev@yahoo.com>
2021-02-09 16:25:18 +0700

    First commit

---
+++ src/CircleArea.java
@@ -0,0 +1,7 @@
+public class CircleArea {
+    public static void main(String[] args) {
+        int radius = 14;
+        double circleArea = Math.PI * Math.pow(radius, 2);
+        System.out.println("The area of circle: " + circleArea);
+    }
+}
